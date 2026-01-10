30cfc3b4a2c2bc10bff35b9034b639115521bd1f
puja khadka <khadkapuja195@gmail.com>
2026-01-10 08:55:22 +0545

    simple like button

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import { useState } from "react";
+import { FaThumbsUp } from "react-icons/fa";
+function App() {
+  const [liked,setLiked]=useState(false)
+  const [count ,setCount]=useState(0)
+  
+  const handleClick=()=>{
+   if(!liked){
+    setLiked(true)
+    setCount(count+1)
+   }else{
+    setLiked(false)
+    setCount(count-1)
+   }
+    
+  }
+
+  return (
+    <>
+      <h1> your liked is :{count}</h1>
+    <div className="flex justify-center items-center h-screen">
+    
+    <div className={`flex justify-center items-center h-10 w-10 ${liked?"bg-blue-600":"bg-gray-600"}`} onClick={handleClick}>
+      <FaThumbsUp className="text-white "/>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
